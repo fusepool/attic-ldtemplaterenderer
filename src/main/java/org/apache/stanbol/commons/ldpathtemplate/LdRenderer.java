27a8dc819d7f4d4db3ae491213a7aa9728e12037
Reto Bachmann-Gmür <reto@apache.org>
2012-09-21 23:53:20 +0200

    getting templates from bundle resources

--- src/main/java/org/apache/stanbol/commons/ldpathtemplate/LdRenderer.java
+++ src/main/java/org/apache/stanbol/commons/ldpathtemplate/LdRenderer.java
@@ -1,75 +1,118 @@
 package org.apache.stanbol.commons.ldpathtemplate;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
 import java.net.URL;
-
-import javax.servlet.ServletOutputStream;
+import java.util.Collection;
+import java.util.HashSet;
 
 import org.apache.clerezza.rdf.core.Resource;
 import org.apache.clerezza.rdf.utils.GraphNode;
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.stanbol.commons.ldpath.clerezza.ClerezzaBackend;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.newmedialab.ldpath.api.backend.RDFBackend;
 import at.newmedialab.ldpath.template.engine.TemplateEngine;
-import freemarker.cache.ClassTemplateLoader;
 import freemarker.cache.TemplateLoader;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 @Component
 @Service(LdRenderer.class)
 public class LdRenderer {
 	
-	private static final Logger log = LoggerFactory.getLogger(LdRenderer.class);
+	private static final String TEMPLATES_PATH_IN_BUNDLES = "templates/";
 
-	public void render(GraphNode node, final URL template, Writer out) {
-		//cfg.setTemplateLoader(new ClassTemplateLoader(getClass(),""));
-		//Configuration cfg = new Configuration();
-		TemplateLoader templateLoader = new TemplateLoader() {
-			
-			@Override
-			public Reader getReader(Object templateSource, String encoding)
-					throws IOException {
-				URL templateUrl = (URL) templateSource;
-				return new InputStreamReader(templateUrl.openStream(), encoding);
-			}
-			
-			@Override
-			public long getLastModified(Object templateSource) {
-				// not known
-				return -1;
-			}
-			
-			@Override
-			public Object findTemplateSource(String name) throws IOException {
-				if ("root".equals(name)) {
-					return template;
-				}
-				log.warn("Template "+name+" not known");
-				return null;
-			}
-			
-			@Override
-			public void closeTemplateSource(Object templateSource) throws IOException {
+	private static final Logger log = LoggerFactory.getLogger(LdRenderer.class);
 	
-				
+	private final Collection<Bundle> bundles = new HashSet<Bundle>();
+	
+	private BundleListener bundleListener = new BundleListener() {
+		
+		@Override
+		public void bundleChanged(BundleEvent event) {
+			if ((event.getType() == BundleEvent.STARTED) && containsTemplates(event.getBundle())) {
+				bundles.add(event.getBundle());
+			} else {
+				bundles.remove(event.getBundle());
 			}
-		};
+		}
+	};
+	
+	private TemplateLoader templateLoader = new TemplateLoader() {
+		
+		@Override
+		public Reader getReader(Object templateSource, String encoding)
+				throws IOException {
+			URL templateUrl = (URL) templateSource;
+			return new InputStreamReader(templateUrl.openStream(), encoding);
+		}
+		
+		@Override
+		public long getLastModified(Object templateSource) {
+			// not known
+			return -1;
+		}
+		
+		@Override
+		public Object findTemplateSource(String name) throws IOException {
+			for (Bundle bundle : bundles) {
+				URL res = bundle.getResource(TEMPLATES_PATH_IN_BUNDLES+name);
+				if (res != null) {
+					return res;
+				}
+			}
+			log.warn("Template "+name+" not known");
+			return null;
+		}
+		
+		@Override
+		public void closeTemplateSource(Object templateSource) throws IOException {
+
+			
+		}
+	};
+	
+	@Activate
+	protected void activate(final ComponentContext context) {
+		final Bundle[] registeredBundles = context.getBundleContext().getBundles();
+		for (int i = 0; i < registeredBundles.length; i++) {
+			if ((registeredBundles[i].getState() == Bundle.ACTIVE) 
+					&& containsTemplates(registeredBundles[i])) {
+				bundles.add(registeredBundles[i]);
+			}
+		}	
+		context.getBundleContext().addBundleListener(bundleListener);
+	}
+
+	@Deactivate
+	protected void deactivate(final ComponentContext context) {
+		context.getBundleContext().removeBundleListener(bundleListener);
+	}
+	
+	private boolean containsTemplates(Bundle bundle) {
+		return bundle.getResource(TEMPLATES_PATH_IN_BUNDLES) != null;
+	}
+
+	public void render(GraphNode node, final String templatePath, Writer out) {	
+		//A GraphNode backend could be graph unspecific, so the same engine could be
+		//resued, possibly being signifantly more performant (caching, etc.)
 		RDFBackend<Resource> backend = new ClerezzaBackend(node.getGraph());
 		Resource context = node.getNode();
 		TemplateEngine<Resource> engine = new TemplateEngine<Resource>(backend);
 		engine.setTemplateLoader(templateLoader);
 		try {
-			engine.processFileTemplate(context, "root", null, out);
+			engine.processFileTemplate(context, templatePath, null, out);
 			out.flush();
 			out.close();
 		} catch (IOException e) {
